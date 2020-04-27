5747a8cd2f9b5c4230e19c2324421e3d702fb112
Baciu Tudor Stefan <47207902+baciutudorstefan@users.noreply.github.com>
2020-04-27 21:47:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var APIKey = '09858877bb2538e70eac2d6d50a1fee25311837a5591af779de312869ecda475';
-var page = '1';
+
+$(document).ready(function(){
+  var page = '1';
 
 
 function getData() {
-  $.getJSON('https://api.unsplash.com/users/baciutudor/photos?per_page=10&page='+page+'&client_id=09858877bb2538e70eac2d6d50a1fee25311837a5591af779de312869ecda475', function(data) {
+  $.getJSON('https://api.unsplash.com/users/baciutudor/photos?per_page=10&order_by=latest&page='+page+'&client_id=09858877bb2538e70eac2d6d50a1fee25311837a5591af779de312869ecda475', function(data) {
 
     console.log(data);
 
@@ -15,20 +16,14 @@ function getData() {
       var image = val;
       var imageURL = val.urls.regular;
       var imageDescription = val.description;
-      var userName = val.user.name;
+      var imgId = val.id;
       var userLocation = val.user.location;
       var userImageLikes = val.likes;
-      var userImageColor = val.color;
       var viewUnsplash = val.links.html;
 
       $('.grid-container').append(
-        '<div class="image"><img src="'+ imageURL +'"><p> ' + imageDescription + '</p>'+ userLocation +'<p> </p> <a href=' + viewUnsplash +' target=_blank> view on unsplash</a></div>'
+        '<div class="image" ><a data-magnify="gallery" href="'+imageURL+'"  ><img id="myModal" src="'+imageURL+'" /></a><div><p> '+imageDescription+'</p><a href=""><span class="iconify" data-icon="ant-design:heart-outlined" data-inline="false"></span><br>'+ userImageLikes + ' </a><br>  <p> <span class="iconify" data-icon="ic:outline-location-on" data-inline="false"></span>'+ userLocation +'</p> <a href=' + viewUnsplash +' target=_blank> view on unsplash</a> </div></div>'
       );  
-      
-//       $("img").hover(function() {
-//   $(".image").css("background-color", userImageColor )
-// });
-      
     });  
   });
 }
@@ -43,3 +38,7 @@ $('div.load-more-button > button').on('click', function() {
   page ++;
   getData();
 });
+
+
+});
+
